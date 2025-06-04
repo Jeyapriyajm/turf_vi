34c514131cc08ca134df103721f977fd0233485d
Jeyapriya Jeyakrishnan <2021t01213@stu.cmb.ac.lk>
2025-06-04 16:21:08 +0530

    update 5

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import "./HeroSection.css";
+import "./Feature3.css";
 import { ChevronDown, Calendar, Clock, MapPin } from "lucide-react";
 
 const HeroSection = ({ backgroundImage, headerText }) => {
@@ -123,11 +124,26 @@ const HeroSection = ({ backgroundImage, headerText }) => {
         </div>
 
         {/* CTA Button */}
-        <a href="/demopage">
-          <button className="cta-container1">
-            <span className="cta-text1">BOOK YOUR TURF</span>
-          </button>
-        </a>
+        <div className="tf3-about-cta">
+          <a href="/demopage">
+            <button className="tf3-cta-button">
+              <span>BOOK YOUR TURF</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </button>
+          </a>{" "}
+        </div>
         {/* Scroll indicator */}
         <div className="scroll-indicator1" onClick={handleScrollDown}>
           <span className="scroll-text1">EXPLORE MORE</span>
